5fedafcca398d0e0b4735d7cf314dcec6b455ab0
brunos01 <brunosperoni01@gmail.com>
2022-09-29 15:27:20 -0300

    finalizando

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,8 +4,35 @@ edad = parseInt(prompt("Bienvenido a la página oficial del CLub Social y Deport
 if (edad >= 18) {
     alert("Puedes ingresar, eres mayor de edad");
 } else { alert("Eres menor de edad, ingresas a la pag. bajo tu responsabilidad") }
-function redirigir() {
-    confirm("Esta seguro que desea abandonar el sitio");
+
+/*Preguntar para salir cuando hacemos click en la red social*/
+function sali() {
+    if (window.confirm("Esta seguro que desea abandonar el sitio")) {
+        window.open("https://www.instagram.com/clublaemilia/", alert("Gracias por su visita!!"));
+    }
+    else {
+        alert("Sigue Navegando en nuestra web ");
+        window.open("index.html");
+
+    }
 }
+function salf() {
+    if (window.confirm("Esta seguro que desea abandonar el sitio")) {
+        window.open("https://es-la.facebook.com/ClubSocialyDeportivoLaEmilia/", alert("Gracias por su visita!!"));
+    }
+    else {
+        alert("Sigue Navegando en nuestra web ");
+        window.open("index.html");
 
+    }
+}
+function salw() {
+    if (window.confirm("Esta seguro que desea abandonar el sitio")) {
+        window.open("https://api.whatsapp.com/send?phone=3364214209&text=Para mas info sobre el club o cualquier duda que tengas comunicate via Whatsapp", alert("Gracias por su visita!!"));
+    }
+    else {
+        alert("Sigue Navegando en nuestra web ");
+        window.open("index.html");
 
+    }
+}
